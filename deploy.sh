28c79d3e2b6e5afb732a021ccf06adf531d512c2
maneav78 <avoyan.mane@mail.ru>
2024-09-26 20:03:39 +0400

    trying to execute deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "hello" || exit 1
+sudo apt-get update
+sudo apt-get install docker.io -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker compose up --build -d
+
