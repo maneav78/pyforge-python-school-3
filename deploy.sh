e140e694ac987d5e87128d98109177644a411878
maneav78 <avoyan.mane@mail.ru>
2024-09-26 23:06:13 +0400

    changed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ubuntu
-
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-
-
 docker-compose up --build -d
-
