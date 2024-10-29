06cf33a6c3d2eb7856833669abd53432da12372a
maneav78 <avoyan.mane@mail.ru>
2024-10-29 20:13:34 +0400

    testing variables

--- deploy.sh
+++ deploy.sh
@@ -33,9 +33,9 @@ echo "MAIN_DATABASE_URL='${MAIN_DATABASE_URL}'" | sudo tee -a /etc/environment
 echo "TEST_DB_NAME='${TEST_DB_NAME}'" | sudo tee -a /etc/environment
 echo "TEST_DATABASE_URL='${TEST_DATABASE_URL}'" | sudo tee -a /etc/environment
 
-source /etc/environment
+. /etc/environment
 
-if [ ! -f docker-compose.yml ]; then
+if [ ! -f docker-compose.yaml ]; then
     echo "docker-compose.yml not found in the current directory. Exiting."
     exit 1
 fi
